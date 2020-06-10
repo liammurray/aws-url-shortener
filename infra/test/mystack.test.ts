4c342186462cc2f35777697645ad5dc66b478a71
Liam Murray <liammurray@users.noreply.github.com>
2020-06-10 11:22:59 -0700

    Update test and snapshot

--- infra/test/mystack.test.ts
+++ infra/test/mystack.test.ts
@@ -6,25 +6,31 @@ import ApiStack from '../src/apiStack'
 
 // TODO expand and add build pipeline stack
 
-test('API stack matches snapshot', () => {
-  const app = new cdk.App()
+describe('API stack', () => {
+  let stack: ApiStack
 
-  const stack = new ApiStack(app, 'urls-api-dev', {
-    certId: 'ssm:/cicd/common/certs/us-west-2',
-    domain: 'ssm:/cicd/common/domain',
-    prefix: 'u',
-    stage: 'dev',
-    env: {
-      account: '12345',
-      region: process.env.AWS_REGION || 'us-west-2',
-    },
-  })
+  beforeAll(() => {
+    const app = new cdk.App()
 
-  expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
-
-  cdkExpect(stack).to(
-    haveResource('AWS::ApiGateway::RestApi', {
-      Name: 'Url Shortener Service',
+    stack = new ApiStack(app, 'urls-api-dev', {
+      certId: 'ssm:/cicd/common/certs/us-west-2',
+      domain: 'ssm:/cicd/common/domain',
+      prefix: 'u',
+      stage: 'dev',
+      env: {
+        account: '12345',
+        region: process.env.AWS_REGION || 'us-west-2',
+      },
     })
-  )
+  })
+  test('API stack matches snapshot', () => {
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot()
+  })
+  test('API stack check', () => {
+    cdkExpect(stack).to(
+      haveResource('AWS::ApiGateway::RestApi', {
+        Name: 'Url Shortener Service',
+      })
+    )
+  })
 })
